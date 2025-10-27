bdc1e6cdf6af86ec75eb0c13490887e00d2da68b
sumanth338 <sumanthashwin000@gmail.com>
2025-10-27 21:55:34 +0530

    Add PUT route for updating hotel details in the hotels API

--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -17,6 +17,20 @@ router.post('/', async(req, res)=>{
     }
 })
 //UPDATE
+router.put("/:id", async (req, res)=>{
+    try{
+        const updateHotel = await Hotel.findByIdAndUpdate(
+            req.params.id,
+            {$set: req.body},
+            {new: true}
+        )
+        res.status(200).json(updateHotel)
+    }
+    catch(error){
+        res.status(500).json(error)
+    }
+})
+
 //DELETE
 //GET
 //GETALL
