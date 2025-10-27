e65458995cb63abfabf525ec8e26b23d0bb79e4e
sumanth338 <sumanthashwin000@gmail.com>
2025-10-27 22:12:05 +0530

    create get and get all methods

--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -9,7 +9,7 @@ router.post('/', async(req, res)=>{
     const newHotel = new Hotel(req.body)
 
     try{
-        const savedHotel = newHotel.save()
+        const savedHotel = await newHotel.save()
         res.status(200).json(savedHotel)
     }
     catch(err){
@@ -34,7 +34,7 @@ router.put("/:id", async (req, res)=>{
 //DELETE
 router.delete("/:id", async (req, res)=>{
     try{
-        const updateHotel = await Hotel.findByIdAndDelete(
+        const deleteHotel = await Hotel.findByIdAndDelete(
             req.params.id,
         )
         res.status(200).json("hotel has been deleted")
@@ -44,7 +44,27 @@ router.delete("/:id", async (req, res)=>{
     }
 })
 //GET
+router.get("/:id", async (req, res)=>{
+    try{
+        const hotel = await Hotel.findById(
+            req.params.id,
+        )
+        res.status(200).json(hotel)
+    }
+    catch(error){
+        res.status(500).json(error)
+    }
+})
 //GETALL
+router.get("/", async (req, res)=>{
+    try{
+        const hotels = await Hotel.find()
+        res.status(200).json(hotels)
+    }
+    catch(error){
+        res.status(500).json(error)
+    }
+})
 
 
 export default router;
